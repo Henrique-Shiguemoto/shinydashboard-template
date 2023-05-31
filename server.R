253493563f3e2c2b9a6a3fda4852b8df6a8bb891
Rick Shiguemoto <shiguemoto88@gmail.com>
2023-05-30 21:25:28 -0300

    Added some sample tabs and also some commented shinyjs code for eventual debugging

--- server.R
+++ server.R
@@ -7,5 +7,6 @@ library(ggplot2)
 library(shinydashboard)
 
 shinyServer(function(input, output){
-    
+	# uncomment this line if you wanna debug with shinyjs
+	# runcodeServer()
 })
--- ui.R
+++ ui.R
@@ -1,15 +1,32 @@
 library(shiny)
 library(shinyjs)
 library(shinycustomloader)
-library(rio)
-library(tidyverse)
-library(ggplot2)
 library(shinydashboard)
+library(leaflet)
+
+# I usually like to use non-white colors for boxes' background color
+background_box_color = "light-blue"
 
 shinyUI(
     dashboardPage(
-        dashboardHeader(),
-        dashboardSidebar(),
-        dashboardBody()
+        dashboardHeader(title = "Dashboard Template"),
+        dashboardSidebar(
+            sidebarMenu(
+                menuItem("Tab1", tabName = "tab1"),
+                menuItem("Tab2", tabName = "tab2"),
+                menuItem("Tab3", tabName = "tab3")
+                # uncomment this line if you wanna debug with shinyjs
+                # useShinyjs(),
+                # runcodeUI(type = "textarea")
+            )
+        ),
+        dashboardBody(
+            tabItems(
+                # Add elements such as boxes, fluidRows and columns after the tabName parameters
+                tabItem(tabName = "tab1"),
+                tabItem(tabName = "tab2"),
+                tabItem(tabName = "tab3")
+            )
+        )
     )
 )
